77aed230bef32e6e6228b69ab4fbf055fef92c9a
quicksandzn <quicksandzn@gmail.com>
2019-11-28 14:00:40 +0800

    init

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import config
 import time
 from gensim.models.word2vec import LineSentence, PathLineSentences
 from gensim.models.callbacks import CallbackAny2Vec
-
+from multiprocessing import cpu_count
 
 class callback(CallbackAny2Vec):
     def __init__(self):
@@ -11,18 +11,19 @@ class callback(CallbackAny2Vec):
 
     def on_epoch_end(self, model):
         loss = model.get_latest_training_loss()
-        print('Loss after epoch {}: {}'.format(self.epoch, loss))
+        print('Loss after epoch {} {}: {}'.format(time.time(),self.epoch, loss))
         self.epoch += 1
 
 
 def train(data_path, model_path, model_txt_path):
-    start_time = start_time = time.time()
+    start_time = time.time()
     # sentences = word2vec.PathLineSentences(segment_dir)
     sentences = LineSentence(data_path)
-    train_model = Word2Vec(sentences, workers=8, iter=20, compute_loss=True, callbacks=[callback()])
+    train_model = Word2Vec(sentences, workers=cpu_count(), iter=20, compute_loss=True, callbacks=[callback()])
+    train_model.callbacks = {}
     train_model.save(model_path)
     train_model.wv.save_word2vec_format(model_txt_path, binary=False)
-    end_time = start_time = time.time()
+    end_time = time.time()
     print("train done", (start_time - end_time))
 
 
