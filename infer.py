8a81b8d3672999d958fb51827f3919d8ce2bbdf8
quicksandzn <quicksandzn@gmail.com>
2019-11-28 14:12:45 +0800

    vector

--- infer.py
+++ infer.py
@@ -8,7 +8,7 @@ def infer(model_path):
     # this load func is too slow
     # model = Word2VecKeyedVectors.load_word2vec_format("/data1/services/word2vec/output/test/all.txt")
     model = Word2Vec.load(model_path)
-    # print vector
+    # vector
     # print(model['北京'])
     print(model.most_similar('北京', topn=20))
     print(model.similarity('重庆市', '青岛市'))
