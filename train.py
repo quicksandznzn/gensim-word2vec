52e350c1b99a987eaa1d70c7ff0d6712dc4e892a
quicksandzn <quicksandzn@gmail.com>
2019-12-03 11:45:58 +0800

    init

--- train.py
+++ train.py
@@ -5,21 +5,23 @@ from gensim.models.word2vec import LineSentence, PathLineSentences
 from gensim.models.callbacks import CallbackAny2Vec
 from multiprocessing import cpu_count
 
+
 class callback(CallbackAny2Vec):
     def __init__(self):
         self.epoch = 0
 
     def on_epoch_end(self, model):
         loss = model.get_latest_training_loss()
-        print('Loss after epoch {} {}: {}'.format(time.strftime('%Y-%m-%d %H:%M:%S %p'),self.epoch, loss))
+        print('Loss after epoch {} {}: {}'.format(time.strftime('%Y-%m-%d %H:%M:%S %p'), self.epoch, loss))
         self.epoch += 1
 
 
 def train(data_path, model_path, model_txt_path):
     start_time = time.time()
     # sentences = word2vec.PathLineSentences(segment_dir)
-    sentences = LineSentence(data_path)
-    train_model = Word2Vec(sentences, workers=cpu_count(), iter=20, compute_loss=True, callbacks=[callback()])
+    # train_model = Word2Vec(LineSentence(data_path), sg=1, window=10, workers=cpu_count(), iter=20, compute_loss=True,
+    #                       callbacks=[callback()])
+    train_model = Word2Vec(LineSentence(data_path), workers=cpu_count(), iter=20, compute_loss=True, callbacks=[callback()])
     train_model.callbacks = {}
     train_model.save(model_path)
     train_model.wv.save_word2vec_format(model_txt_path, binary=False)
